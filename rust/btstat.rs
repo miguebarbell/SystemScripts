8a0fd16c2edfaa08b2217ebd499168f3c0ca3881
Miguel <contact@migue.pro>
2022-01-16 17:15:32 -0500

    changed folder to Rust

--- rust/btstat.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-
-fn main() {
-  // let mut stat = Command::new("bluetooth --help | egrep -o 'on|off'")
-  let mut stat = Command::new("/usr/bin/bluetooth");
-  stat.arg("--help");
-  println!(stat);
-}
