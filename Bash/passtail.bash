cb0b103aa035cdfa264d3a7721d4b807cba03610
Miguel <contact@migue.pro>
2022-01-22 21:00:36 -0500

    passmenu

---
+++ Bash/passtail.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+path="$1"
+passfile="$PREFIX/$path.gpg"
+check_sneaky_paths "$path"
+
+if [[ -f $passfile ]]; then
+    $GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +2 || exit $?
+elif [[ -z $path ]]; then
+    die ""
+else
+    die "Error: $path is not in the password store."
+fi
