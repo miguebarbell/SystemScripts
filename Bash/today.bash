78841a6979032d95d5b909d7ee695d2944200f44
Miguel <miguel@debloat.us>
2022-04-26 14:05:11 -0400

    backup working in artix

--- Bash/today.bash
+++ Bash/today.bash
@@ -3,7 +3,7 @@
 # verify if the last line contain todays date, and if not, fetch it from internet.
 CACHE_QUOTE="$HOME/.cache/todayquote"
 if [ ! -f $CACHE_QUOTE ]; then touch $CACHE_QUOTE; fi
-if [ $(date +"%D") != $(tail -n 1 $CACHE_QUOTE) ]; then
+if [[ $(date +"%D") != $(tail -n 1 $CACHE_QUOTE) ]]; then
 	#sed -i '$ d' $CACHE_QUOTE
 	truncate -s 0 $CACHE_QUOTE
 	curl -s https://today.zenquotes.io | grep 'id="api-data-1"' | grep -o '>.*<' | sed -e 's/\&#[0-9]*/''/g' -e 's/>/''/g' -e 's/</''/g' -e 's/;[0-9]*;/''/g' -e 's/\s;/,/'> $CACHE_QUOTE
