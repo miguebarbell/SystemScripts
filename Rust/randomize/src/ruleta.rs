21971683f3212efd65dae35b1dec190380aedcd9
Miguel R <miguel@debloat.us>
2022-10-19 20:53:50 -0400

    ruleta with test

--- Rust/randomize/src/ruleta.rs
+++ Rust/randomize/src/ruleta.rs
@@ -1,5 +1,11 @@
+use rand::Rng;
+
 pub fn ruleta(options: Vec<&String>) -> &String {
-    options.get(0).unwrap()
+    // options.get(0).unwrap()
+
+    let mut rng = rand::thread_rng();
+    options[rng.gen_range(0..options.len())]
+    // choose(&mut rand::thread_rng())
 }
 
 #[cfg(test)]
@@ -9,5 +15,15 @@ mod ruleta_test {
     fn one_option() {
         let blue: String = "blue".to_string();
         assert_eq!(ruleta([&"blue".to_string()].to_vec()), &blue)
+    }
+
+    #[test]
+    fn flag_colors() {
+        let blue = "blue".to_string();
+        let white = "white".to_string();
+        let red = "red".to_string();
+        let flag_colors: Vec<&String> = [&blue, &white, &red].to_vec();
+        let color = ruleta(flag_colors.clone());
+        assert!(flag_colors.contains(&color))
     }
 }
