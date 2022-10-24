f1870bd3a63ef327f64273fb2a7c9206d6ebced3
Miguel R <miguel@debloat.us>
2022-10-24 09:23:43 -0400

    added color as option

--- Rust/unread_emails/src/main.rs
+++ Rust/unread_emails/src/main.rs
@@ -5,6 +5,13 @@ fn main() {
     let mut args = env::args().collect::<Vec<String>>();
     args.remove(0);
     let mut emails_path = String::new();
+    let mut colored = false;
+    for (i, arg) in args.clone().iter().enumerate() {
+        if arg == "-c" {
+            colored = true;
+            args.remove(i);
+        }
+    }
     if !args.is_empty() {
         emails_path.push_str(args.join(" ").as_str())
     } else {
@@ -16,13 +23,21 @@ fn main() {
     };
     let unread_emails_count = fs::read_dir(&emails_path).unwrap().count();
     if unread_emails_count > 0 {
-        println!(
-            // "\nYou have \x1b[93m{} \x1b[0munread emails.",
-            "\nYou have \x1b[6;30;42m{}\x1b[0m \x1b[1munread emails.\x1b[0m",
-            unread_emails_count
-        )
+        if colored {
+            println!(
+                // "\nYou have \x1b[93m{} \x1b[0munread emails.",
+                "\nYou have \x1b[6;30;42m{}\x1b[0m \x1b[1munread emails.\x1b[0m",
+                unread_emails_count
+            )
+        } else {
+            println!("\nYou have {} unread emails.", unread_emails_count)
+        }
     } else {
-        println!("\n\x1b[0;32;44mNo new emails.\x1b[0m")
+        if colored {
+            println!("\n\x1b[0;32;44mNo new emails.\x1b[0m")
+        } else {
+            println!("\nNo new emails.")
+        }
     }
 
     for file in files {
@@ -51,7 +66,11 @@ fn main() {
             }
         }
         if (has_subject) && (has_from) {
-            println!("\x1b[1;34;42m{:?}\x1b[0m\n{:?}\n", from, subject);
+            if colored {
+                println!("\x1b[1;34;42m{:?}\x1b[0m\n{:?}\n", from, subject);
+            } else {
+                println!("{:?}\n{:?}\n", from, subject);
+            }
         }
     }
 }
