b29f63710ca39681d0379da252a7520039d9ebe4
Miguel R <miguel@debloat.us>
2022-11-09 16:04:24 -0500

    ready

--- Bash/bookmarks.bash
+++ Bash/bookmarks.bash
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# check for the file, if not create i
+clean_file() {
+    CLEANED_DATA=$(cat $1 | uniq)
+    echo "$CLEANED_DATA" > "$1"
+}
+# check for the file, if not create it
 FILE="$XDG_DATA_HOME"/bookmarks
 ADD="ADD NEW ENTRY"
-if [ ! -f "$FILE" ] || [ -e "$FILE" ]; then
-	touch $FILE
+if [ ! -f "$FILE" ]; then
+	touch "$FILE"
 	echo "$ADD" > "$FILE"
+else
+	  clean_file "$FILE"
 fi
 
-SELECTION=$(cat $FILE | rofi -dmenu -p 'Select for copy')
+SELECTION=$(cat "$FILE" | rofi -dmenu -p 'Select for copy')
 
 if [ "$SELECTION" = "$ADD" ]; then
 	NEW_BOOKMARK=$(rofi -dmenu -p 'Adding new Bookmark')
-	# echo -e $NEW_BOOKMARK
-	NEW="$(cat $FILE)\n$NEW_BOOKMARK\n$ADD"
-	# echo -e $NEW | rofi -dmenu -p 
-	echo -e $NEW
-	truncate -s 0 $FILE
-	echo -e "$NEW" >| tee -a "$FILE"
-	# sh $0
+	echo -e "$NEW_BOOKMARK" >> "$FILE"
+	 sh "$0"
 elif [ "$SELECTION" = "" ]; then
 	# clean exit
 	exit 0
 else
-	xdotool type $SELECTION
-	echo $SELECTION | xclip
+	xdotool type "$SELECTION"
+	echo "$SELECTION" | xclip
 	exit 0
 fi
-
