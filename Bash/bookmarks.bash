73b33b91ab8270eeff4c8862ba3eb32d99bfd6dc
Miguel <miguel@debloat.us>
2023-06-17 10:10:24 -0400

    bookmarksync

--- Bash/bookmarks.bash
+++ Bash/bookmarks.bash
@@ -2,20 +2,21 @@
 
 # NOTE: type edit or add to add/edit a new entry
 clean_file() {
-    CLEANED_DATA=$(cat $1 | uniq)
-    echo "$CLEANED_DATA" > "$1"
+	CLEANED_DATA=$(cat $1 | uniq)
+	echo "$CLEANED_DATA" >"$1"
 }
 # check for the file, if not create it
-FILE="$XDG_DATA_HOME"/bookmarks
+# FILE="$XDG_DATA_HOME"/bookmarks
+FILE="$XDG_SYNC_HOME/bookmarks"
 SYNC_FOLDER="/home/$USER/pCloudDrive/Miguel/sync/"
 sync() {
 	rsync "$FILE" "$SYNC_FOLDER"
 }
 if [ ! -f "$FILE" ]; then
 	touch "$FILE"
-	echo "$HELP" > "$FILE"
+	echo "$HELP" >"$FILE"
 else
-	  clean_file "$FILE"
+	clean_file "$FILE"
 fi
 
 SELECTION=$(sed '1i use ADD or EDIT' "$FILE" | rofi -dmenu -p 'Select for copy')
@@ -23,20 +24,20 @@ SELECTION=$(sed '1i use ADD or EDIT' "$FILE" | rofi -dmenu -p 'Select for copy')
 # NOTE: Everytime you add or edit the file, it will try to sync it
 if [ "$SELECTION" = "add" ]; then
 	NEW_BOOKMARK=$(rofi -dmenu -p 'Adding new Bookmark')
-	echo -e "$NEW_BOOKMARK" >> "$FILE"
-	sync
-	 sh "$0"
- elif [ "$SELECTION" = "edit" ]; then
-	 $TERMINAL -e "$EDITOR" "$FILE"
-	 $SELECTION
-	 sync
-	 sh "$0"
+	echo -e "$NEW_BOOKMARK" >>"$FILE"
+	# sync
+	sh "$0"
+elif [ "$SELECTION" = "edit" ]; then
+	$TERMINAL -e "$EDITOR" "$FILE"
+	$SELECTION
+	# sync
+	sh "$0"
 elif [ "$SELECTION" = "" ]; then
 	# clean exit
 	exit 0
 else
 	# echo "$SELECTION"
 	xdotool type "$SELECTION"
-	echo "$SELECTION" | xclip 
+	echo "$SELECTION" | xclip
 	exit 0
 fi
